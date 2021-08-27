45f015ad8419e6a17d2dcddecffcf10b6fb869a5
TranHungKt <tranhung26122612@gmail.com>
2021-08-27 17:20:48 +0700

    setup provider

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,17 +2,21 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
+import { Provider } from 'react-redux';
 import Home from '@modules/home/index';
+import store from '../redux/Store';
 
 const Stack = createNativeStackNavigator();
 
 function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={Home} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <Provider store={store}>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Home" component={Home} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </Provider>
   );
 }
 
