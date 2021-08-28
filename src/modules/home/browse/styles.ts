56c0059695c6ca8543496e9f5ca98efa7e7984ff
Phu Le <phu_le@epam.com>
2021-08-28 20:21:56 +0700

    move screen

--- src/modules/home/browse/styles.ts
+++ src/modules/home/browse/styles.ts
@@ -11,4 +11,10 @@ export default StyleSheet.create({
     alignItems: 'center',
   },
   screen: { width: '100%', height: '100%', backgroundColor: White },
+  descriptionWrapper: { flexDirection: 'row', justifyContent: 'flex-start', marginTop: 10 },
+  descriptionTextWrapper: { flex: 3 },
+  brandText: { fontWeight: 'bold' },
+  numOfSaveText: { marginTop: 5, fontWeight: 'bold' },
+  dotdotdotWrapper: { flex: 1 },
+  dotdotdot: { alignSelf: 'flex-end' },
 });
--- src/modules/social/updates/index.tsx
+++ src/modules/social/updates/index.tsx
@@ -1,14 +1,49 @@
 import React from 'react';
-import { SafeAreaView, View } from 'react-native';
-import UpdateScreen from './screen';
+import { FlatList, SafeAreaView, View } from 'react-native';
+import UpdateCard from '../../../components/UpdateCard';
+import { useAppSelector } from '../../../redux/HookRedux';
 import styles from './styles';
 
-const Updates = () => (
-  <SafeAreaView style={styles.centerContainer}>
-    <View style={styles.screen}>
-      <UpdateScreen />
-    </View>
-  </SafeAreaView>
-);
+const Updates = () => {
+  const { users } = useAppSelector((state) => state.users);
+  const { challenges } = useAppSelector((state) => state.challenges);
+
+  const propList = [
+    {
+      userId: users[2].id,
+      challengeId: challenges[3].id,
+      userRelation: true,
+    },
+    {
+      userId: users[1].id,
+      challengeId: challenges[5].id,
+    },
+    {
+      userId: users[0].id,
+      challengeId: challenges[0].id,
+    },
+  ];
+
+  return (
+    <SafeAreaView style={styles.centerContainer}>
+      <View style={styles.screen}>
+        <FlatList
+          style={{ marginTop: 10 }}
+          horizontal={false}
+          showsVerticalScrollIndicator={false}
+          data={propList}
+          keyExtractor={(item, index) => index.toString()}
+          renderItem={({ item }) => (
+            <UpdateCard
+              userId={item.userId}
+              challengeId={item.challengeId}
+              userRelation={item.userRelation}
+            />
+          )}
+        />
+      </View>
+    </SafeAreaView>
+  );
+};
 
 export default Updates;
