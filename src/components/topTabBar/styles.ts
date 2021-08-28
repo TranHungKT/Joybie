0fd78c7e472f4b1822d36842cbbf86af68c908e4
Nghia Nguyen <nnnghia98@gmail.com>
2021-08-28 15:47:53 +0700

    fix the style of top tabs bar

--- src/components/topTabBar/styles.ts
+++ src/components/topTabBar/styles.ts
@@ -2,22 +2,23 @@ import { StyleSheet, Platform, Dimensions } from 'react-native';
 import { Colors } from '../../styles/index';
 
 const styles = StyleSheet.create({
+  containerBgr: {
+    backgroundColor: Colors.White,
+  },
   tabContainer: {
-    marginTop: 12,
-    marginBottom: 12,
     flexDirection: 'row',
     justifyContent: 'center',
     alignItems: 'center',
     alignSelf: 'center',
-    height: 46,
+    backgroundColor: Colors.White,
+    height: 60,
     width: Dimensions.get('window').width / 1.75,
     fontSize: 16,
-    elevation: 1,
   },
   slider: {
     height: 3,
     position: 'absolute',
-    top: 40,
+    top: 45,
     left: 33,
     backgroundColor: Colors.Purple,
   },
@@ -25,7 +26,6 @@ const styles = StyleSheet.create({
     flex: 1,
     paddingBottom: Platform.OS === 'ios' ? 15 : 0,
     width: (Dimensions.get('window').width * 13) / 25 / 2,
-    height: 50,
     alignItems: 'center',
     justifyContent: 'center',
     flexDirection: 'row',
