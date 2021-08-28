bbbb292a41364ef175171e5a8d86fb06f6260b9a
Phu Le <phu_le@epam.com>
2021-08-28 20:29:57 +0700

    move detail screen

--- src/modules/detail/index.tsx
+++ src/modules/detail/index.tsx
@@ -1,14 +1,67 @@
-import React from 'react';
-import { SafeAreaView, View } from 'react-native';
-import DetailScreen from './screen';
+import React, { useRef } from 'react';
+import {
+  SafeAreaView, View,
+  Image, Text, ScrollView, TouchableOpacity,
+} from 'react-native';
+
+import { useRoute, RouteProp } from '@react-navigation/native';
+
+import SlidingUpPanel from 'rn-sliding-up-panel';
+import { RootStackParams } from '../../routes/RoutesParams';
+import { useAppSelector } from '../../redux/HookRedux';
 import styles from './styles';
 
-const Detail = () => (
-  <SafeAreaView style={styles.centerContainer}>
-    <View style={styles.screen}>
-      <DetailScreen />
-    </View>
-  </SafeAreaView>
-);
+import { SlidePanel } from '../../components';
+
+type DetailScreenProps = RouteProp<RootStackParams, 'DetailScreen'>;
+
+const Detail = () => {
+  const route = useRoute<DetailScreenProps>();
+  const { id } = route.params;
+  const { challenges } = useAppSelector((state) => state.challenges);
+
+  const challengeIdx = challenges.findIndex((challenge) => challenge.id === id);
+
+  const refPanel = useRef<SlidingUpPanel>(null);
+
+  const showPanel = () => refPanel.current?.show();
+  return (
+    <SafeAreaView style={styles.centerContainer}>
+      <View style={styles.screen}>
+        <ScrollView style={styles.scrollView} showsVerticalScrollIndicator={false}>
+          <View style={styles.wrapper}>
+            <Image
+              style={styles.image}
+              source={{ uri: challenges[challengeIdx].imageUrl }}
+            />
+            <View style={styles.challengeDetails}>
+              <Text style={styles.challengeDetailsText}>Challenge Details</Text>
+            </View>
+            <View style={styles.textWrapper}>
+              <View style={styles.textContainer}>
+                <Text style={styles.titleText}>{challenges[challengeIdx].title}</Text>
+                <Text style={styles.numOfDoneText}>{`${challenges[challengeIdx].numOfDone} people did this`}</Text>
+                <Text style={styles.descriptionText}>{challenges[challengeIdx].description}</Text>
+              </View>
+            </View>
+            <View style={styles.buttonWrapper}>
+              <View style={{ flex: 1 }}>
+                <View style={styles.joinChallenge}>
+                  <Text style={styles.buttonText}>Join challenge</Text>
+                </View>
+              </View>
+              <View style={{ flex: 1 }}>
+                <TouchableOpacity style={styles.share} onPress={showPanel}>
+                  <Text style={styles.buttonText}>Share</Text>
+                </TouchableOpacity>
+              </View>
+            </View>
+          </View>
+        </ScrollView>
+        <SlidePanel refPanel={refPanel} />
+      </View>
+    </SafeAreaView>
+  );
+};
 
 export default Detail;
