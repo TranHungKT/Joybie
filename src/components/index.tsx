434ba8bca153c2e70256b238dfd84ff4848050a0
TranHungKt <tranhung26122612@gmail.com>
2021-08-28 11:50:16 +0700

    modal

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,9 +1,11 @@
 import MyTabBar from './tabBar';
 import SlidePanel from './slidePanel';
 import MyTopTabBar from './topTabBar';
+import Modal from './Modal';
 
 export {
   MyTabBar,
   SlidePanel,
-  MyTopTabBar
+  MyTopTabBar,
+  Modal,
 };
--- src/modules/profile/index.tsx
+++ src/modules/profile/index.tsx
@@ -1,10 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
-  SafeAreaView,
   TextInput,
   View,
   ScrollView,
 } from 'react-native';
+import { Modal } from '@components/index';
 import styles from './styles';
 import Header from './components/Header';
 import { Colors } from '../../styles';
@@ -13,23 +13,27 @@ import Rewards from './components/Rewards';
 
 const userId = 1;
 
-const Profile = () => (
-  <ScrollView showsVerticalScrollIndicator={false} style={styles.centerContainer} contentContainerStyle={styles.content}>
-    <Header userId={userId} />
-    <View style={styles.textInputView}>
-      <TextInput
-        placeholder="Search"
-        placeholderTextColor={Colors.Black}
-        style={styles.textInput}
-      />
-    </View>
-    <View style={{ flex: 1 }}>
-      <Challenges userId={userId} />
-    </View>
-    <View style={{ flex: 1 }}>
-      <Rewards userId={userId} />
-    </View>
-  </ScrollView>
-);
+const Profile = () => {
+  const [isVisible, setIsVisible] = useState(true);
+  return (
+    <ScrollView showsVerticalScrollIndicator={false} style={styles.centerContainer} contentContainerStyle={styles.content}>
+      <Modal isVisible={isVisible} setIsVisible={setIsVisible} />
+      <Header userId={userId} />
+      <View style={styles.textInputView}>
+        <TextInput
+          placeholder="Search"
+          placeholderTextColor={Colors.Black}
+          style={styles.textInput}
+        />
+      </View>
+      <View style={{ flex: 1 }}>
+        <Challenges userId={userId} />
+      </View>
+      <View style={{ flex: 1 }}>
+        <Rewards userId={userId} />
+      </View>
+    </ScrollView>
+  );
+};
 
 export default Profile;
